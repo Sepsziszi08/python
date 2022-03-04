42edea8e81c918f9c0e8826259805315f78daf12
Szép Szilárd <szeps20@ADY-NAGYATAD.SULINET.HU>
2022-03-04 11:27:27 +0100

    3. feladat

--- 500/otszaz.py
+++ 500/otszaz.py
@@ -1,1 +1,14 @@
+f=open("penztar.txt")
+kosar=[]
+#szoveg=f.read()
+#print(szoveg)
+for sor in f:
+    kosar.append(sor.strip())
+f.close()
 
+print("2. feladat")
+print("Fizetések száma: " + str(kosar.count("F")))
+
+
+print("3. feladat")
+print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárlolt.")
