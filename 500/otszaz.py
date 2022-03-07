3afda4d3dafe67131d8bb3f130bddfa3f6a1d99c
Szép Szilárd <szeps20@ADY-NAGYATAD.SULINET.HU>
2022-03-07 08:43:37 +0100

    ötödik feladat

--- 500/otszaz.py
+++ 500/otszaz.py
@@ -12,3 +12,18 @@ print("Fizetések száma: " + str(kosar.count("F")))
 
 print("3. feladat")
 print("Az első vásárló " + str(kosar.index("F")) + " darab árucikket vásárlolt.")
+
+
+sorszam=int(input("Vásárlás sorszáma: "))
+arunev=input("Árucikk neve: ")
+darab=int(input("Darabszám: "))
+
+aruindex=kosar.index(arunev)
+darablista=kosar[:aruindex]
+print(darablista)
+vasarlasdb=darablista.count("F") +1
+print (vasarlasdb)
+
+print("5. feladat")
+print("Az első vásárlás sorszáma: " +str(vasarlasdb)
+
